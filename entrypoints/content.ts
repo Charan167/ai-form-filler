4fd0f6132da92226263db211b4ae332fc66d54a1
Charan167 <charan4224@gmail.com>
2025-09-20 18:05:02 +0000

    feat: add event listener only to visibile text area elements

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,28 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["*://*.google.com/*"],
+  main(ctx) {
+    const textAreaElements = document.querySelectorAll(
+      "textarea, [contenteditable=true]"
+    );
+
+    const visibletextAreaElements = Array.from(textAreaElements).filter(
+      (element) => {
+        const styles = window.getComputedStyle(element);
+
+        return (
+          styles.display !== "none" &&
+          styles.opacity !== "0" &&
+          element.clientWidth > 0
+        );
+      }
+    );
+
+    visibletextAreaElements.forEach((element) => {
+      element.addEventListener(
+        "focus",
+        () => console.log("focus is working")
+      );
+      element.addEventListener("input", () => console.log("input is working"));
+    });
   },
 });
