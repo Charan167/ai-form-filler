fe1ed1420dcddd4d63283c726b253592c4b9ebd1
DhyanTD <dhyan@smoketrees.in>
2025-09-26 00:20:19 +0530

    fix: allow extension all urls

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,27 +1,27 @@
 export default defineContentScript({
   matches: ["*://*"],
   main(ctx) {
-    // console.log(getInputs());
-    const textAreaElements = document.querySelectorAll(
-      "textarea, [contenteditable=true]",
-    );
-
-    const visibletextAreaElements = Array.from(textAreaElements).filter(
-      (element) => {
-        const styles = window.getComputedStyle(element);
-
-        return (
-          styles.display !== "none" &&
-          styles.opacity !== "0" &&
-          element.clientWidth > 0
-        );
-      },
-    );
-
-    visibletextAreaElements.forEach((element) => {
-      element.addEventListener("focus", () => console.log("focus is working"));
-      element.addEventListener("input", () => console.log("input is working"));
-    });
+    console.log("hello", getInputs());
+    // const textAreaElements = document.querySelectorAll(
+    //   "textarea, [contenteditable=true]",
+    // );
+    //
+    // const visibletextAreaElements = Array.from(textAreaElements).filter(
+    //   (element) => {
+    //     const styles = window.getComputedStyle(element);
+    //
+    //     return (
+    //       styles.display !== "none" &&
+    //       styles.opacity !== "0" &&
+    //       element.clientWidth > 0
+    //     );
+    //   },
+    // );
+    //
+    // visibletextAreaElements.forEach((element) => {
+    //   element.addEventListener("focus", () => console.log("focus is working"));
+    //   element.addEventListener("input", () => console.log("input is working"));
+    // });
   },
 });
 
