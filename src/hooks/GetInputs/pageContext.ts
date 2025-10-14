62836f4c0e26ebfc985beea2e29a581a18e3a3dd
Charan167 <charan4224@gmail.com>
2025-10-14 18:08:56 +0000

    feat: add parent and page context

---
+++ src/hooks/GetInputs/pageContext.ts
@@ -0,0 +1,16 @@
+export function getPageContext() {
+  const pageContext: any[] = [];
+  const elements = document.querySelectorAll(
+    "head > title, meta[type='description'], body > h1 "
+  );
+
+  elements.forEach((element) =>
+    pageContext.push({
+      tag: element?.tagName,
+      id: element?.id,
+      innerText: element?.innerHTML,
+    })
+  );
+
+  return pageContext;
+}
